5f7020ef10fa467307c95b6b41fde39543cb0f70
Paulo Bu <pbu_98@yahoo.com>
2015-07-10 13:08:33 +0200

    Better absolute path calculation for templates folder

--- src/my_blog/core.clj
+++ src/my_blog/core.clj
@@ -1,21 +1,25 @@
+(use 'ring.util.response)
+(use 'selmer.parser)
+
 (ns my-blog.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]))
 
-(use 'ring.util.response)
-(use 'selmer.parser)
-
-(selmer.parser/set-resource-path! "/Users/pbu/clojure-web/clj-blog/src/my_blog/templates/")
+; root of the project source
+(def ROOT 
+  (.getCanonicalPath (clojure.java.io/file ".")))
+; set templates folder
+(selmer.parser/set-resource-path! 
+  (clojure.string/join "/" [ROOT "src/my_blog/templates/"]))
+; turns off template cache for development
 (selmer.parser/cache-off!)
 
-(defn hello [request]
-  (render-file "index.html" {}))
-
+; routes
 (defroutes app
-  (GET "/" [] hello)
+  (GET "/" [] home)
   (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
 
-(defn handler [request]
-  (-> (response "Hello World")
-      (content-type "text/html")))
+; views
+(defn home [request]
+  (render-file "index.html" {}))

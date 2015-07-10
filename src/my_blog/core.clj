b1f87beef748144e2cc8f6a7f0af9b76f0ee44c8
Paulo Bu <pbu_98@yahoo.com>
2015-07-10 16:53:40 +0200

    Improved path handling and code restructured

--- src/my_blog/core.clj
+++ src/my_blog/core.clj
@@ -1,25 +1,29 @@
-(use 'ring.util.response)
-(use 'selmer.parser)
-
 (ns my-blog.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]))
+
+(use 'ring.util.response)
+(use 'selmer.parser)
+
+; util functions
+(defn path-join [path1 path2]
+  (str (java.nio.file.Paths/get path1 (into-array String [path2]))))
 
 ; root of the project source
 (def ROOT 
   (.getCanonicalPath (clojure.java.io/file ".")))
 ; set templates folder
 (selmer.parser/set-resource-path! 
-  (clojure.string/join "/" [ROOT "src/my_blog/templates/"]))
+  (path-join ROOT "src/my_blog/templates/"))
 ; turns off template cache for development
 (selmer.parser/cache-off!)
+
+; views
+(defn home [request]
+  (render-file "index.html" {}))
 
 ; routes
 (defroutes app
   (GET "/" [] home)
   (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
-
-; views
-(defn home [request]
-  (render-file "index.html" {}))

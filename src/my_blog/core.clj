cbe8e2bf415583db21c93a9be5ef84cc42dedb90
Paulo Bu <pbu_98@yahoo.com>
2015-08-04 17:04:33 +0200

    Moves templates to resources folder

--- src/my_blog/core.clj
+++ src/my_blog/core.clj
@@ -5,25 +5,12 @@
 (use 'ring.util.response)
 (use 'selmer.parser)
 
-; util functions
-(defn path-join [path1 path2]
-  (str (java.nio.file.Paths/get path1 (into-array String [path2]))))
-
-; root of the project source
-(def ROOT 
-  (.getCanonicalPath (clojure.java.io/file ".")))
-; set templates folder
-(selmer.parser/set-resource-path! 
-  (path-join ROOT "src/my_blog/templates/"))
-; turns off template cache for development
-(selmer.parser/cache-off!)
-
 ; views
 (defn home [request]
-  (render-file "index.html" {:nav_active "home"}))
+  (render-file "templates/index.html" {:nav_active "home"}))
 
 (defn about [request]
-  (render-file "about.html" {:nav_active "about"}))
+  (render-file "templates/about.html" {:nav_active "about"}))
 
 ; routes
 (defroutes app
